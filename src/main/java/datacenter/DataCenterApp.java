e34a2c6b1ef310e1c314c53f9f534f88eb5f03df
Milica Bucko <milica.milica.b@gmail.com>
2018-01-15 20:20:38 +0100

    Initial commit

---
+++ src/main/java/datacenter/DataCenterApp.java
@@ -0,0 +1,12 @@
+package datacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCenterApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCenterApp.class, args);
+    }
+}
